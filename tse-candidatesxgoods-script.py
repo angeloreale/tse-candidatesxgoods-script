d8914229a303d44bee8a385711403a13310c8b69
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2018-09-27 03:56:50 -0300

    Second commit, adding comments.

--- tse-candidatesxgoods-script.py
+++ tse-candidatesxgoods-script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
+# In[2] importing libs:
 
 
 import pandas as pd
@@ -16,7 +16,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 get_ipython().run_line_magic('pylab', 'inline')
 
 
-# In[151]:
+# In[3] fetching and merging data:
 
 
 dfConsulta = pd.read_csv('./data/candidatos-consulta/consulta_cand_2018_BRASIL.csv', sep=";", encoding = 'latin1', error_bad_lines=False)
@@ -27,37 +27,37 @@ dfMerged = dfConsulta.merge(dfBens, on='SQ_CANDIDATO')
 dfMerged.head()
 
 
-# In[114]:
+# In[4] filtering data:
 
 
 dfMergedFilteredBens = dfMerged.filter(items=['NM_URNA_CANDIDATO','NR_PARTIDO','SG_PARTIDO','SQ_CANDIDATO','VR_BEM_CANDIDATO','CD_COR_RACA','DS_COR_RACA','CD_GRAU_INSTRUCAO','DS_GRAU_INSTRUCAO','CD_GENERO','DS_GENERO','NR_IDADE_DATA_POSSE','CD_CARGO','DS_CARGO','SG_UE_y'])
 
 
-# In[115]:
+# In[5] formatting currency:
 
 
 dfMergedFilteredBens["VR_BEM_CANDIDATO"] = dfMergedFilteredBens["VR_BEM_CANDIDATO"].str.replace(',','.').astype(float)
 
 
-# In[116]:
+# In[6] summing goods:
 
 
 dfMergedFilteredBensSum = dfMergedFilteredBens.groupby(['NM_URNA_CANDIDATO','NR_PARTIDO','SG_PARTIDO','SQ_CANDIDATO','CD_COR_RACA','DS_COR_RACA','CD_GRAU_INSTRUCAO','DS_GRAU_INSTRUCAO','CD_GENERO','DS_GENERO','NR_IDADE_DATA_POSSE','CD_CARGO','DS_CARGO','SG_UE_y'])['VR_BEM_CANDIDATO'].sum().reset_index()
 
 
-# In[117]:
+# In[7] exporting to csv:
 
 
 dfMergedFilteredBensSum.to_csv('/home/angeloreale/candidatura-redebahia/teste-python/dados/dfMergedFilteredBensSum.csv', encoding = 'latin1')
 
 
-# In[118]:
+# In[8] previewing processed data:
 
 
 dfMergedFilteredBensSum.describe()
 
 
-# In[119]:
+# In[9] drawing histograms:
 
 
 fig = plt.figure(figsize=(12, 6))
@@ -75,7 +75,7 @@ goods.set_title("Histogram of Candidate's Goods")
 plt.show()
 
 
-# In[120]:
+# In[10] processing statistics and displaying them:
 
 
 import statsmodels.api as sm
@@ -85,59 +85,52 @@ m = ols('VR_BEM_CANDIDATO ~ NR_IDADE_DATA_POSSE + CD_COR_RACA + CD_GENERO + CD_G
 print (m.summary())
 
 
-# In[122]:
+# In[11] drawing plot:
 
 
 sns.jointplot(x="NR_IDADE_DATA_POSSE", y="VR_BEM_CANDIDATO", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
 plt.show()
 
 
-# In[124]:
-
-
-sns.jointplot(x="CD_GENERO", y="NR_IDADE_DATA_POSSE", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
-plt.show()
-
-
-# In[125]:
+# In[13] drawing plot:
 
 
 sns.jointplot(x="CD_GENERO", y="VR_BEM_CANDIDATO", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
 plt.show()
 
 
-# In[127]:
+# In[14] drawing plot:
 
 
 sns.jointplot(x="CD_COR_RACA", y="VR_BEM_CANDIDATO", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
 plt.show()
 
 
-# In[128]:
+# In[15] drawing plot:
 
 
 sns.jointplot(x="CD_GRAU_INSTRUCAO", y="VR_BEM_CANDIDATO", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
 plt.show()
 
 
-# In[129]:
+# In[16] drawing plot:
 
 
 sns.jointplot(x="CD_CARGO", y="VR_BEM_CANDIDATO", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
 plt.show()
 
 
-# In[147]:
+# In[17] drawing plot:
+
+
+sns.jointplot(x="NR_PARTIDO", y="VR_BEM_CANDIDATO", data=dfMergedFilteredBensSum, kind = 'reg',fit_reg= True, size = 7)
+plt.show()
+
+
+# In[147] drawing and saving plot:
 
 
 with sns.axes_style(style='ticks'):
     g = sns.factorplot("CD_COR_RACA", "VR_BEM_CANDIDATO", "DS_GENERO", data=dfMergedFilteredBensSum, size=10, kind="box")
     g.set_axis_labels("Etnia", "Bens");
     g.savefig('factorplot01.png')
-
-
-# In[ ]:
-
-
-
-
